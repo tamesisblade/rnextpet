7ec4e1110b7ec142ae2ec14f734950ea6382800a
tamesis <hitlerblade123456@gmail.com>
2021-03-10 00:11:28 +0100

    introduccion4

--- components/home/SliderHome.js
+++ components/home/SliderHome.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Layout from './SliderHome'
 import styles from '../../styles/Home.module.css'
+import HomeSliderResponsive from './utils/HomeSliderResponsive'
 function SliderHome() {
     return (
-        <div>
+    <div>
      
        
      <div id="carouselExampleIndicators"  className="carousel slide" data-bs-ride="carousel">
@@ -13,24 +14,27 @@ function SliderHome() {
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={1} aria-label="Slide 2" />
          
         </div>
-
-        <div className="carousel-inner" >
+       
+       {/*HOME SLIDER RESPONSIVE*/}
+        <HomeSliderResponsive/>
+       {/*END HOME SLIDER RESPONSIVE*/}
+        <div className="carousel-inner d-none d-sm-none" >
           <div className="carousel-item active " >
             <img src="./img/home/fondohome.jpg" className="d-block w-100 home-slider-img"  alt="..." />
             <div className="carousel-caption d-none d-md-block ">
 
-                 <div className="word-slider" style={{width:"200px", position:"relative",bottom:"120px",right:"50px"}}>
+                 <div className="" style={{width:"200px", position:"relative",bottom:"120px",right:"50px"}}>
                         <p style={{fontSize:"18px"}}>Sale Up To 10%</p>
                 </div>
 
-                <div className="word-slider" style={{width:"600px" , position:"relative",bottom:"130px",right:"60px"}}>
+                <div className="" style={{width:"600px" , position:"relative",bottom:"130px",right:"60px"}}>
                         <p  style={{fontSize:"60px"}}>Accesories For Pet</p>
                 </div>
 
-                <div className="word-slider " style={{width:"550px" , position:"relative",bottom:"130px",right:"25px"}}>
+                <div className=" " style={{width:"550px" , position:"relative",bottom:"130px",right:"25px"}}>
                         <p  style={{fontSize:"20px"}}>Make your Pet's Day With Our Range Of Pet accessories at Petoria today. Our Pet Bandamas are handmade to the highest quality for you pet!</p>
                 </div>
-                <div className="word-slider " style={{width:"200px" , position:"relative",bottom:"120px",right:"60px"}}>
+                <div className=" " style={{width:"200px" , position:"relative",bottom:"120px",right:"60px"}}>
                        <button style={{position:"relative", left:"10px",color:"#fff", background:"none",border:"1px #fff solid",padding:"15px"}}>VIEW NOW</button>
                 </div>
             </div>
@@ -60,6 +64,7 @@ function SliderHome() {
          
          
         </div>
+        
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true" />
           <span className="visually-hidden">Previous</span>
